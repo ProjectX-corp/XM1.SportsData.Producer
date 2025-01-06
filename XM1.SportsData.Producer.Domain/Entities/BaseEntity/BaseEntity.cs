2eac243420ca5fa78201d3931f43c3f63013bbee
dotnetGUILHERMECAMPOS <139664669+dotnetGUILHERMECAMPOS@users.noreply.github.com>
2025-01-06 14:38:34 -0300

    Adicionar arquivos de projeto.

---
+++ XM1.SportsData.Producer.Domain/Entities/BaseEntity/BaseEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XM1.SportsData.Producer.Domain.Entities.BaseEntity
+{
+    internal class BaseEntity
+    {
+    }
+}
