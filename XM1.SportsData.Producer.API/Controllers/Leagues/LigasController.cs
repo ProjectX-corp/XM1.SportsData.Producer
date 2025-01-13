53698fe47bb400e3489cfeeee3bc5ab92eff5bd9
guilherme-xmatch <139664669+guilherme-xmatch@users.noreply.github.com>
2025-01-13 01:11:58 -0300

    Ajuste

---
+++ XM1.SportsData.Producer.API/Controllers/Leagues/LigasController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace XM1.SportsData.Producer.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LigasController : ControllerBase
+    {
+    }
+}
