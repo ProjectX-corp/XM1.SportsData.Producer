4d600e1892cfb1bc8e04447921526fa121975638
guilherme-xmatch <139664669+guilherme-xmatch@users.noreply.github.com>
2025-01-13 01:10:06 -0300

    Ajuste das novas configurações

---
+++ XM1.SportsData.Producer.API/Controllers/PaisController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace XM1.SportsData.Producer.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaisController : ControllerBase
+    {
+
+    }
+}
