996a340bf418f7a5a147002e6c38d59d40007230
guilherme-xmatch <139664669+guilherme-xmatch@users.noreply.github.com>
2025-01-13 01:19:22 -0300

    Update Partidas.cs

--- XM1.SportsData.Producer.Domain/Entities/Fixtures/Partidas.cs
+++ XM1.SportsData.Producer.Domain/Entities/Fixtures/Partidas.cs
@@ -19,6 +19,6 @@ namespace XM1.SportsData.Producer.Domain.Entities.Fixtures
         public int? GolsTimeCasa { get; set; }
         public int? GolsTimeVisitante { get; set; }
         public bool? IsResultadoCompleto { get; set; }
-
+        public bool? AoVivo { get; set; }
     }
 }
